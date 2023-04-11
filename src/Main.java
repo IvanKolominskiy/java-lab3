412cdb93b253c07389c5faccf964fd5813da5fb9
Ivan Kolominskiy <ifkolominskiy@edu.hse.ru>
2023-04-11 20:49:21 +0300

    Main function has been changed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter name surname patronymic and date of birth");
+        String[] inputTokens = scanner.nextLine().split(" ");
+
+        if (inputTokens.length != 4) {
+            System.out.println("Wrong input");
+        } else {
+            //TODO
+        }
     }
 }
